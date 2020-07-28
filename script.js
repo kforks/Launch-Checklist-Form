d46e1b4bc60e4a95a528ce764ee34c0faac3adfd
Kaitlyn forks <kaitlynforks@VT-100541.local>
2020-07-27 21:21:02 -0500

    all except JSON

--- script.js
+++ script.js
@@ -1,13 +1,42 @@
 // Write your JavaScript code here!
 
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
-<h2>Mission Destination</h2>
-<ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
-</ol>
-<img src="${}">
-*/
+window.addEventListener("load", function() {
+   let form = document.querySelector("form");
+   form.addEventListener("submit", function(event) {
+      event.preventDefault();
+      let pilotInput = document.querySelector("input[name=pilotName]");
+      let copilotInput = document.querySelector("input[name=copilotName]");
+      let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
+      let cargoMassInput = document.querySelector("input[name=cargoMass]");
+
+         if (pilotInput.value === "" || copilotInput.value === "" || fuelLevelInput.value === "" || cargoMassInput.value === "") {
+            alert("All fields are required!");
+         }
+         else if ( !isNaN(pilotInput.value) || !isNaN(copilotInput.value) ) {
+            alert("Pilot or copilot name is invalid. Names must contain letters.");
+         }
+         else if ( isNaN(fuelLevelInput.value) || isNaN(cargoMassInput.value) ) {
+            alert("Fuel level and cargo mass must be numbers.");
+         }
+         else if ( fuelLevelInput.value < 10000) {
+            faultyItems.style.visibility = 'visible' ;
+            document.getElementById("launchStatus").innerHTML = 'The shuttle is not ready.';
+            document.getElementById("launchStatus").style.color = "red";
+            document.getElementById("fuelStatus").innerHTML = "Fuel level too low.";
+         }
+         else if ( cargoMassInput.value > 10000) {
+            faultyItems.style.visibility = 'visible' ;   
+            document.getElementById("launchStatus").innerHTML = "The shuttle is not ready."
+            document.getElementById("launchStatus").style.color = "red";
+            document.getElementById("cargoStatus").innerHTML = "The shuttle is too heavy.";
+         }
+         else {
+            faultyItems.style.visibility = 'visible'
+            document.getElementById("launchStatus").innerHTML = "Shuttle is ready for launch!";
+            launchStatus.style.color = "green";
+         }
+         document.getElementById("pilotStatus").innerHTML =  `${pilotInput.value} ready!`
+         document.getElementById("copilotStatus").innerHTML =  `${copilotInput.value} ready!`
+      
+   });
+});
