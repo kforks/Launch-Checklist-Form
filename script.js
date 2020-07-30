08d8306e2224741a76bb50b013fb308780e92d2f
Kaitlyn forks <kaitlynforks@VT-100541.local>
2020-07-29 19:54:27 -0500

    final version

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Write your JavaScript code here!
 
+
 window.addEventListener("load", function() {
    let form = document.querySelector("form");
    form.addEventListener("submit", function(event) {
@@ -45,26 +46,54 @@ window.addEventListener("load", function() {
       document.getElementById("pilotStatus").innerHTML =  `${pilotInput.value} ready!`
       document.getElementById("copilotStatus").innerHTML =  `${copilotInput.value} ready!`
       
-      //JSON fetch for mission destination;
-      //window.addEventListener("load", function() {
-         fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {
-            response.json().then( function(json) {
-               document.getElementById("missionTarget").innerHTML = `
-               <h2>Mission Destination</h2>
-               <ol>
-                  <li>Name: ${json[0].name}</li>
-                  <li>Diameter: ${json[0].diameter}</li>
-                  <li>Star: ${json[0].star}</li>
-                  <li>Distance from Earth: ${json[0].distance}</li>
-                  <li>Number of Moons: ${json[0].moons}</li>
-               </ol>
-               <img src="${json[0].image}">
-               `;
-            });
-         });
-      //});
    });
 });
+
+
+//JSON fetch for mission destination;
+window.addEventListener("load", function() {  
+   fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {
+      response.json().then( function(json) {
+         const mission = document.getElementById("missionTarget");
+         mission.innerHTML = `
+         <h2>Mission Destination</h2>
+         <ol>
+            <li>Name: ${json[0].name}</li>
+            <li>Diameter: ${json[0].diameter}</li>
+            <li>Star: ${json[0].star}</li>
+            <li>Distance from Earth: ${json[0].distance}</li>
+            <li>Number of Moons: ${json[0].moons}</li>
+         </ol>
+         <img src="${json[0].image}">
+         `;
+      });
+   });
+});
+
+
+//JSON fetch for mission destination;
+// window.onLoad = function() {
+//    console.log("noodles")
+//    fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {
+//       response.json().then( function(json) {
+//          const mission = document.getElementById("missionTarget");
+
+         
+
+//          mission.innerHTML = `
+//          <h2>Mission Destination</h2>
+//          <ol>
+//             <li>Name: ${json[0].name}</li>
+//             <li>Diameter: ${json[0].diameter}</li>
+//             <li>Star: ${json[0].star}</li>
+//             <li>Distance from Earth: ${json[0].distance}</li>
+//             <li>Number of Moons: ${json[0].moons}</li>
+//          </ol>
+//          <img src="${json[0].image}">
+//          `;
+//       });
+//    });
+// };
 
 
   
