10c88eb081e24f4fa5bb0951d088cb4cb5ad511c
Kaitlyn forks <kaitlynforks@VT-100541.local>
2020-07-29 21:35:35 -0500

    deleted commented out code

--- script.js
+++ script.js
@@ -70,30 +70,4 @@ window.addEventListener("load", function() {
    });
 });
 
-
-//JSON fetch for mission destination;
-// window.onLoad = function() {
-//    console.log("noodles")
-//    fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {
-//       response.json().then( function(json) {
-//          const mission = document.getElementById("missionTarget");
-
-         
-
-//          mission.innerHTML = `
-//          <h2>Mission Destination</h2>
-//          <ol>
-//             <li>Name: ${json[0].name}</li>
-//             <li>Diameter: ${json[0].diameter}</li>
-//             <li>Star: ${json[0].star}</li>
-//             <li>Distance from Earth: ${json[0].distance}</li>
-//             <li>Number of Moons: ${json[0].moons}</li>
-//          </ol>
-//          <img src="${json[0].image}">
-//          `;
-//       });
-//    });
-// };
-
-
   
